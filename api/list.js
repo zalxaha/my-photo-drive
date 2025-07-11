0b22cfd9d1d0db5dfc70c9cc0fe8e1d52b03595f
zalxaha <cariuangonline54@gmail.com>
2025-07-11 23:35:45 +0700

    Update list.js

--- api/list.js
+++ api/list.js
@@ -22,17 +22,18 @@ router.get('/api/list', async (req, res) => {
     const files = data
       .filter(f => f.type === 'file')
       .map(f => {
-        const [timestamp, ...rest] = f.name.split('-');
+        const [maybeTimestamp, ...rest] = f.name.split('-');
         const name = rest.join('-') || f.name;
-        const tsNumber = Number(timestamp);
-        const isValid = !isNaN(tsNumber) && tsNumber > 1e12; // pastikan ini timestamp ms
+
+        const isTimestamp = /^\d{13}$/.test(maybeTimestamp); // valid timestamp in ms
+        const tsNumber = isTimestamp ? Number(maybeTimestamp) : null;
 
         return {
           name,
           url: `https://raw.githubusercontent.com/${GH_OWNER}/${GH_REPO}/${BRANCH}/uploads/${f.name}`,
           isImage: f.name.match(/\.(png|jpe?g|gif|webp)$/i),
           isVideo: f.name.match(/\.(mp4|webm|mov)$/i),
-          date: isValid ? new Date(tsNumber).toISOString() : null
+          date: tsNumber ? new Date(tsNumber).toISOString() : null
         };
       });
 
